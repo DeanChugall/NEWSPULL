e7f0bb60aca1aa4792b74ea2a8fa0390117637dc
Dean <deanchugall@gmail.com>
2018-05-14 00:35:19 +0200

    Initial

---
+++ app/src/main/java/club/androidx/mvc/model/utilities/Constants.java
@@ -0,0 +1,9 @@
+package club.androidx.mvc.model.utilities;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://newsapi.org/v2/";
+    public static final String PHOTO_URL = BASE_URL + "/photos/";
+
+}
+
